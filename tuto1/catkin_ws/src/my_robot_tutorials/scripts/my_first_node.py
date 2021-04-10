948922b99b563a43adff73dbbf0469b477f4276c
gsion <gontran.sion@gmail.com>
2021-04-10 13:31:10 -0400

    feat: add tuto folder, with 2 basic node in python and CPP

---
+++ tuto1/catkin_ws/src/my_robot_tutorials/scripts/my_first_node.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import rospy
+
+if __name__ == '__main__':
+	rospy.init_node('my_first_python_node')
+
+	rospy.loginfo("This node has been started")
+
+	rate = rospy.Rate(1)
+
+	while not rospy.is_shutdown():
+		rospy.loginfo("Hello")
+		rate.sleep()
